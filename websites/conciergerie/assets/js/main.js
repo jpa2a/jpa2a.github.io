f870c1d47132c066f61584c80008681df2a90cfb
jean paul andreani <jpandreani@gmail.com>
2025-08-10 10:33:55 +0200

    changement anim

--- websites/conciergerie/assets/js/main.js
+++ websites/conciergerie/assets/js/main.js
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     },
     {
-      threshold: 1 // 10% of element must be visible to trigger
+      threshold: 0.5 // 10% of element must be visible to trigger
     }
   );
 
